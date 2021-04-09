21717768c0dc9d0894e7e6fdc740c73ffc7b5f8d
David Thong Nguyen <dev@davidtn.com>
2021-04-08 22:41:17 -0400

    Subscription does not get restarted; just pause the time decrease

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,22 +31,25 @@ export class AppComponent {
   sessionRunning = false;
   sessionLength = 25;
   $sessionTimer;
+  sessionTimerSubscription: Subscription;
   sessions: Session[] = [];
   currSession: Session = null;
   currTaskName: string = '';
 
-  private timeLeft = 0;
+  private timeLeft = null;
   fillHeight = 0;
   fillColor = '#7de891';
   timerPaused = true;
 
   constructor() {
     this.$sessionTimer = interval(1000).pipe(
-      takeWhile(() => !this.timerPaused && this.timeLeft > 0),
+      takeWhile(() => this.sessionRunning),
       map((e) => {
-        this.timeLeft -= 1;
-        const origTime = this.sessionLength * 60;
-        this.fillHeight = (e / origTime) * 100;
+        if (!this.timerPaused) {
+          this.timeLeft -= 1;
+          const origTime = this.sessionLength * 60;
+          this.fillHeight = (e / origTime) * 100;
+        }
         return e;
       })
     );
@@ -76,24 +79,31 @@ export class AppComponent {
     // start timer
     if (!this.timerPaused) {
       if (this.timeLeft > 0) {
-      } else {
+        console.log(`Resumed: time left = ${this.timeLeft}s`);
+      } else if (this.timeLeft == null) {
+        this.sessionRunning = true;
         const minutes: number = this.sessionLength;
         this.timeLeft = minutes * 60;
+        this.currSession = new Session();
+        this.startObservableTimer(this.sessionTimerSubscription);
       }
-      this.startObservableTimer(this.$sessionTimer);
     } else {
-      this.clearObservableTimer(this.$sessionTimer);
+      console.log(`Paused: time left = ${this.timeLeft}s`);
+      // console.log(this.sessionTimerSubscription.);
+      // this.clearObservableTimer(this.sessionTimerSubscription);
     }
     // this.timerPaused ? this.$timer.unsubscribe() : this.$timer.subscribe();
     // const remainingTime = this.timeLeft;
     // console.log(remainingTime);
   }
 
-  private startObservableTimer($timer: Observable<number>) {
-    $timer.subscribe(
-      (e) => console.log(e),
+  private startObservableTimer(sub: Subscription) {
+    this.sessionTimerSubscription = this.$sessionTimer.subscribe(
+      (e) => null,
       (err) => console.error(err),
       () => {
+        console.log(this.timeLeft);
+
         if (this.timeLeft <= 0 || !this.sessionRunning) {
           this.wrapUpSession();
         }
@@ -102,20 +112,25 @@ export class AppComponent {
     );
   }
 
-  private clearObservableTimer($timer: Observable<number>) {
-    // $timer.unsubscribe();
+  private clearObservableTimer(sub: Subscription) {
+    // sub.unsubscribe();
   }
 
   public wrapUpSession() {
+    if (this.timeLeft == this.sessionLength * 60) {
+      return;
+    }
+    console.log('Wrapping up session');
     this.currSession = {
       taskName: this.currTaskName,
       sessionElapsedTime: this.sessionLength * 60 - this.timeLeft,
     };
     this.sessions.push(this.currSession);
+    this.sessionRunning = false;
 
     // reset vars
     this.currSession = null;
-    this.timeLeft = 0;
+    this.timeLeft = null;
     this.currTaskName = '';
   }
 
@@ -139,7 +154,6 @@ export class AppComponent {
   }
 
   get timeRemainingRendered() {
-    // return this.$
     return this.renderTimeHmmss(this.timeLeft);
   }
 }
